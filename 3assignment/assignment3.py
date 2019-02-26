e1e9a73d4c247d550a30cd6a0ea76927fe26b1e0
joel <hedwigg01@gmail.com>
2019-02-25 20:38:50 -0700

    Most MnMs are found as white disks, need to replace with color as described in pdf

--- 3assignment/assignment3.py
+++ 3assignment/assignment3.py
@@ -2,17 +2,40 @@ import cv2
 import numpy
 
 
-imgOne = cv2.imread("one.jpg", cv2.IMREAD_COLOR)
+imgOne = cv2.imread("four.jpg", cv2.IMREAD_COLOR)
 cv2.imshow("img", imgOne)
 
-#Technique 1
+#Technique 1:
 
-#blur and canny edge detector
-blurOne =cv2.blur(imgOne, (5,5))
-edgesOne = cv2.Canny(blurOne,100,200)
 
-cv2.imshow("edgesOne", edgesOne)
+#guassian blur (seems to work better) *** 2 blurs works well for img1, 2nd 5x5 mask blur sort of messes up images 3 and 4
+blurOne = cv2.GaussianBlur(imgOne,(5,5),0)
+blurOne = cv2.GaussianBlur(blurOne,(3,3),0)
 
+
+#possibly dialating and eroding first
+kernel = numpy.ones((5,5),numpy.uint8)
+#dialate
+#blurOne = cv2.dilate(blurOne,kernel,iterations = 2)
+#erode
+#edgesOne = cv2.erode(blurOne,kernel,iterations = 2)
+
+#canny edge detection
+edgesOne = cv2.Canny(blurOne,5,150)
+
+cv2.imshow("canny edges", edgesOne)
+
+#finding clircles 
+#started with circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+#parameters here: https://docs.opencv.org/3.1.0/dd/d1a/group__imgproc__feature.html#ga47849c3be0d0406ad3ca45db65a25d2d
+circles = cv2.HoughCircles(edgesOne,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=20,minRadius=20,maxRadius=40) #leave max radius at 40 (perfect for img 4)
+circles = numpy.uint16(numpy.around(circles))
+
+for i in circles[0,:]:
+    cv2.circle(edgesOne,(i[0],i[1]),i[2]-5,(255,255,255), -1) #-1 is to fill in the circles. 255's are for white. i[2]-5 is for drawing 5px smaller than orig circle. ensures circles are not overlapping.
+    
+
+cv2.imshow('detected circles',edgesOne)
 
 
 
