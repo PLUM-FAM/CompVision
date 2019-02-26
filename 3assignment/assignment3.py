b070ec0d7c99a85a8323159cf0908090ff59ac75
joel <hedwigg01@gmail.com>
2019-02-25 18:10:09 -0700

    project start and canny edges done

--- 3assignment/assignment3.py
+++ 3assignment/assignment3.py
@@ -2,3 +2,19 @@ import cv2
 import numpy
 
 
+imgOne = cv2.imread("one.jpg", cv2.IMREAD_COLOR)
+cv2.imshow("img", imgOne)
+
+#Technique 1
+
+#blur and canny edge detector
+blurOne =cv2.blur(imgOne, (5,5))
+edgesOne = cv2.Canny(blurOne,100,200)
+
+cv2.imshow("edgesOne", edgesOne)
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
